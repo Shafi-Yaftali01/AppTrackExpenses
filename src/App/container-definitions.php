6dd0ca68b4b1958aaa4f07e77022b2fac9971fef
Shafi Yaftali <143145567+Shafi-Yaftali01@users.noreply.github.com>
2024-08-11 01:38:45 -0400

    Added configuration for login and logout page

--- src/App/container-definitions.php
+++ src/App/container-definitions.php
@@ -2,9 +2,12 @@
 
 declare(strict_types=1);
 
-use Framework\{TemplateEngine, Database};
+use Framework\{TemplateEngine, Database, Container};
 use App\Config\Paths;
-use App\Services\ValidatorService;
+use App\Services\{
+  ValidatorService,
+  UserService
+};
 
 return [
   TemplateEngine::class => fn() => new TemplateEngine(Paths::VIEW),
@@ -13,5 +16,10 @@ return [
     'host' => $_ENV['DB_HOST'],
     'port' => $_ENV['DB_PORT'],
     'dbname' => $_ENV['DB_NAME']
-  ], $_ENV['DB_USER'], $_ENV['DB_PASS'])
+  ], $_ENV['DB_USER'], $_ENV['DB_PASS']),
+  UserService::class => function (Container $container) {
+    $db = $container->get(Database::class);
+
+    return new UserService($db);
+  }
 ];
--- src/Framework/Router.php
+++ src/Framework/Router.php
@@ -16,7 +16,8 @@ class Router
     $this->routes[] = [
       'path' => $path,
       'method' => strtoupper($method),
-      'controller' => $controller
+      'controller' => $controller,
+      'middlewares' => []
     ];
   }
 
@@ -48,13 +49,14 @@ class Router
         $container->resolve($class) :
         new $class;
 
-      $action = fn () => $controllerInstance->{$function}();
+      $action = fn() => $controllerInstance->{$function}();
 
-      foreach ($this->middlewares as $middleware) {
+      $allMiddleware = [...$route['middlewares'], ...$this->middlewares];
+      foreach ($allMiddleware as $middleware) {
         $middlewareInstance = $container ?
           $container->resolve($middleware) :
           new $middleware;
-        $action = fn () => $middlewareInstance->process($action);
+        $action = fn() => $middlewareInstance->process($action);
       }
 
       $action();
@@ -66,5 +68,11 @@ class Router
   public function addMiddleware(string $middleware)
   {
     $this->middlewares[] = $middleware;
+  }
+
+  public function addRouteMiddleware(string $middleware)
+  {
+    $lastRouteKey = array_key_last($this->routes);
+    $this->routes[$lastRouteKey]['middlewares'][] = $middleware;
   }
 }
